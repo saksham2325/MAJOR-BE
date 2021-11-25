c4fc5bb8c0ceacdad88878c1a1cbd4d8ea54e801
sakshamjtg <saksham.garg@joshtechnologygroup.com>
2021-11-25 19:58:08 +0530

    add UserJiraToken model

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,8 @@ from accounts import constants as cc
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=cc.FIRST_NAME_LAST_NAME_MAX_LENGTH)
+    first_name = models.CharField(
+        max_length=cc.FIRST_NAME_LAST_NAME_MAX_LENGTH)
     last_name = models.CharField(max_length=cc.FIRST_NAME_LAST_NAME_MAX_LENGTH)
 
     USERNAME_FIELD = 'email'
@@ -22,7 +23,8 @@ class User(AbstractUser):
 class Group(models.Model):
     admin = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=cc.GROUP_TITLE_MAX_LENGTH, unique=True)
-    description = models.CharField(max_length=cc.GROUP_DESCRIPTION_MAX_LENGTH, blank=True)
+    description = models.CharField(
+        max_length=cc.GROUP_DESCRIPTION_MAX_LENGTH, blank=True)
 
     def __str__(self):
         return self.title
@@ -35,3 +37,9 @@ class UserGroup(models.Model):
 
 class GroupInvitation(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+
+class UserJiraToken(models.Model):
+    user = models.ForeignKey(User)
+    jira_token = models.CharField(max_length=100, unique=True)
+    expiry = models.DateTimeField(null=True)
