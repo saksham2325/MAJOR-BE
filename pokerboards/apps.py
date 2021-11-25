2c86b3d2843275cbd8bacb3af4219f0e606f9d58
Ankur <ankur.gupta@joshtechnologygroup.com>
2021-11-25 19:18:02 +0530

    pokerboards models

---
+++ pokerboards/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PokerboardsConfig(AppConfig):
+    name = 'pokerboards'
---
+++ pokerboards/constant.py
@@ -0,0 +1,19 @@
+ESTIMATE_TYPE = {
+    "fibonacci": 0,
+    "odd": 1,
+    "even": 2,
+    "serial": 3, 
+    "custom": 4,
+}
+
+ROLE = {
+    "player": 0,
+    "spectator": 1,
+}
+
+POKERBOARD_NAME_MAX_LENGTH = 50
+ESTIMATE_MAX_DIGITS = 17
+ESTIMATE_DECIMAL_PLACES = 3
+ESTIMATE_MAX_VALUE = 1e+14
+ESTIMATE_MIN_VALUE = 0.001
+TIMER_DEFAULT_MINUTES = 20
